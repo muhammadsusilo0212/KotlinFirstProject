7c4ce8ea1cb7229abbd0fa10013715857cebefc9
arsilcomp <arsilcomputerservice@gmail.com>
2025-09-07 19:31:04 +0700

    literal pertama

---
+++ src/B2BlokdanKomentar.kt
@@ -0,0 +1,11 @@
+fun main (args: Array<String>){
+    println("Hello, Ini program saya di Kotlin")
+    //Contoh komentar satu baris
+    /*
+    Contooh Komentarr multii baris
+     */
+    /**
+     * Kotlin Doc .. -->> baris ini untuk medokumentasikan kode program yang dibuat
+     */
+
+}
