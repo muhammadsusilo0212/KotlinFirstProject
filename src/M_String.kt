b64abeeff9dff7e60be25d7a09750da966c55dd8
arsilcomp <arsilcomputerservice@gmail.com>
2025-09-08 15:27:40 +0700

    string

---
+++ src/M_String.kt
@@ -0,0 +1,16 @@
+fun main(args: Array<String>) {
+    val myName: String = "muhammad susilo"
+    for(chr in myName){
+        print(chr)
+    }
+    print('\n')
+
+    println("------------------------")
+
+
+    /// contoh  menggabungkan string dengan tipe data lain
+    val s = "abc" +1 +3
+    val g = 2
+    println(s + g +"def")
+    /// contoh  menggabungkan string dengan tipe data lain
+}
