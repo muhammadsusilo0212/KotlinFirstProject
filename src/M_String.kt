144431d3e12e1679b0b583da78b0797eb2295b58
arsilcomp <arsilcomputerservice@gmail.com>
2025-09-08 15:41:29 +0700

    string

--- src/M_String.kt
+++ src/M_String.kt
@@ -13,4 +13,22 @@ fun main(args: Array<String>) {
     val g = 2
     println(s + g +"def")
     /// contoh  menggabungkan string dengan tipe data lain
-}
+
+    println("------------------------")
+
+        //contoh menyimpan data String yang memuat keyword Kotlin
+        var text = """ 
+        for (c in "foo") 
+           print(c) 
+    """
+        println(text)
+
+    var text2 = """ 
+            |Tell me and I forget. 
+            |Teach me and I remember. 
+            |Involve me and I learn. 
+            |(Benjamin Franklin) 
+            """.trimMargin()
+    println(text2)
+
+    }
